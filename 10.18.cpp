a5b9efa3ef75d929a894c40a0c93834e51a0edee
gawenliu <71703277+gawenliu@users.noreply.github.com>
2020-10-19 21:00:19 +0800

    Add files via upload

---
+++ 10.18.cpp
@@ -0,0 +1,33 @@
+/*难度一
+编程实现幂运算：用户输入多组a和b，当输入0 0时输入结束；依次输出a的b次方(结果为int型)
+输入样例：
+3 3
+2 2
+0 0
+输出样例：
+27
+4
+
+难度二
+编程实现幂运算，用户输入一组a和b；输出a的b次方的后两位(注意：a的b次方的结果为超大整数)
+输入样例:
+16 520
+输出样例：
+76
+
+难度三
+使用 快速幂 的方法实现难度二*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	double num1=1,num2,sum;
+	while(num1!=0&&num2!=0)
+	{
+		scanf("%lf",&num1);
+		scanf("%lf",&num2);
+		sum=pow(num1,num2);
+		printf("%g",sum);
+	 } 
+	return 0;
+}
